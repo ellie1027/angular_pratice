b81139a4a3297620f10e72368859a3d0b8c13701
hanbyul9 <beammeupplzz@gmail.com>
2021-01-06 10:43:56 +0900

    [#1] 배열에 insert/delete 간단히 해보기 / 데이터 바인딩 정리

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
 
+// interface Person {
+//   name: string;
+//   age: number;
+// }
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ang01';
+  // title = 'ang01';
+  // myInput = '';
+  // checkedValue1 = 'show';
+  // checkedValue2 = 'show';
+  //
+  // people: Person[] = [
+  //   {name: 'hyuk', age: 33},
+  //   {name: 'byul', age: 31}
+  // ];
+  //
+  // alert(): void {
+  //   alert(this.title);
+  // }
+  value = '';
+  values: string[] = [];
+
+  insertVal(val: string): void {
+    if (val !== ''){
+      this.values.push(val);
+    }
+  }
+
+  deleteVal(idx: number): void {
+    this.values.splice(idx, 1);
+  }
+
 }
